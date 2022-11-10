c41222b9bc4c41e73acc428128e08838cea335b7
Yaotian-Liu <yaotian_liu@qq.com>
2022-11-10 22:38:02 +0800

    feat: 🎸 Implement PostProcess & Post_top

--- src/PostProcess.v
+++ src/PostProcess.v
@@ -1,19 +1,60 @@
-module PostProcess #(parameter POX = 3) (
+module PostProcess #(
+  parameter POX      = 3,
+  parameter INT_BITS = 3
+) (
   input                   clk                  ,
   input                   rst                  ,
   input      [POX*16-1:0] mux_postprocess_data ,
   input                   mux_postprocess_valid,
   input      [POX*16-1:0] K                    ,
   input      [POX*16-1:0] B                    ,
+  input      [POX*16-1:0] bias                 ,
+  output reg [POX*16-1:0] relu_out             ,
+  output reg              relu_out_valid       ,
   output reg [POX*16-1:0] post_out             ,
   output reg              post_out_valid
 );
 
+  wire [POX*16-1:0] relu_out_next;
+
+  wire [POX*16-1:0] post_out_next;
+  wire [POX*16-1:0] add_result   ;
+  wire [POX*32-1:0] mul_results  ; // wire
+
   genvar pox;
   generate
     for (pox = 0; pox < POX; pox = pox + 1) begin
-      ;
+      // Cycle 1
+      assign add_result[(pox+1)*16-1:pox*16]    = mux_postprocess_data[(pox+1)*16-1:pox*16] + bias;
+      assign relu_out_next[(pox+1)*16-1:pox*16] =
+        (add_result[(pox+1)*16-1] == 1'b0) ? add_result[(pox+1)*16-1:pox*16] : 16'b0; // ReLU
+
+      // Cycle 2
+      assign mul_results[(pox+1)*32-1:pox*32]   = relu_out[(pox+1)*16-1:pox*16] * K;
+      assign post_out_next[(pox+1)*16-1:pox*16] =
+        mul_results[(pox+1)*32-1-INT_BITS:pox*32+(16-INT_BITS)] + B;
     end
   endgenerate
+
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      post_out <= 0;
+      relu_out <= 0;
+
+      relu_out_valid <= 0;
+      post_out_valid <= 0;
+    end
+    else begin
+      relu_out <= relu_out_next;
+      post_out <= post_out_next;
+
+      // Need 2 cycles to complete:
+      // 1. Add bias + ReLU
+      // 2. Batch Normalization
+      relu_out_valid <= mux_postprocess_valid;
+      post_out_valid <= relu_out_valid;
+    end
+  end
+
 
 endmodule
---
+++ src/Post_top.v
@@ -0,0 +1,78 @@
+module Post_top #(
+  parameter INT_BITS  = 4,
+  parameter POX       = 3,
+  parameter POY       = 3,
+  parameter CHANNEL_N = 2
+) (
+  input                             clk             ,
+  input                             rst             ,
+  input  [CHANNEL_N*POY*POX*16-1:0] mac_to_serialzer,
+  input                             mac_output_valid,
+  input  [              POX*16-1:0] K               ,
+  input  [              POX*16-1:0] B               ,
+  input  [              POX*16-1:0] bias            ,
+  output [              POX*16-1:0] relu_out        ,
+  output                            relu_out_valid  ,
+  output [              POX*16-1:0] post_out        ,
+  output                            post_out_valid
+);
+
+  wire [CHANNEL_N*POX*16-1:0] all_serializer_out;
+
+  wire [$clog2(CHANNEL_N)-1:0] mux_sel        ;
+  wire [        CHANNEL_N-1:0] mux_sel_one_hot;
+
+  wire [POX*16-1:0] mux_postprocess_data ;
+  wire              mux_postprocess_valid;
+
+  Mux #(
+    .CHANNEL_N(CHANNEL_N),
+    .POX      (POX      ),
+    .POY      (POY      )
+  ) U_Mux (
+    .clk               (clk                  ),
+    .rst               (rst                  ),
+    .all_serializer_out(all_serializer_out   ),
+    .mac_output_valid  (mac_output_valid     ),
+    .mux_sel           (mux_sel              ),
+    .mux_out           (mux_postprocess_data ),
+    .mux_out_valid     (mux_postprocess_valid)
+  );
+
+  genvar c;
+  generate
+    for (c = 0; c < CHANNEL_N; c = c + 1) begin
+      assign mux_sel_one_hot[c] = (mux_sel == c);
+
+      Serializer #(
+        .POX(POX),
+        .POY(POY)
+      ) U_Serializer (
+        .clk             (clk                                              ),
+        .rst             (rst                                              ),
+        .mac_output      (mac_to_serialzer[(c+1)*POY*POX*16-1:c*POY*POX*16]),
+        .mac_output_valid(mac_output_valid                                 ),
+        .mux_sel         (mux_sel                                          ),
+        .serializer_out  (all_serializer_out[(c+1)*POX*16-1:c*POX*16]      )
+      );
+    end
+  endgenerate
+
+  PostProcess #(
+    .POX     (POX     ),
+    .INT_BITS(INT_BITS)
+  ) U_PostProcess (
+    .clk                  (clk                  ),
+    .rst                  (rst                  ),
+    .mux_postprocess_data (mux_postprocess_data ),
+    .mux_postprocess_valid(mux_postprocess_valid),
+    .K                    (K                    ),
+    .B                    (B                    ),
+    .bias                 (bias                 ),
+    .relu_out             (relu_out             ),
+    .relu_out_valid       (relu_out_valid       ),
+    .post_out             (post_out             ),
+    .post_out_valid       (post_out_valid       )
+  );
+
+endmodule
